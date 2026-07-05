aa0d683438632298cd716a719cc016b583b0c6c5
OVERLORD <adityadeshmukh1992007@gmail.com>
2026-07-05 20:47:00 +0530

    Update solution - CodeHub

--- 6-kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.py
+++ 6-kyu/sum-of-digits-slash-digital-root/sum-of-digits-slash-digital-root.py
@@ -1,7 +1,8 @@
 def digital_root(n):
     while len(str(n))>1:
-        total = 0
+        sum = 0
         for digits in str(n):
-            total += int(digits)
-            n = total
-    return n # your code here
+            sum += int(digits)
+            
+            n = sum 
+    return n 
